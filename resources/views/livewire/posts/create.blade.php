95d9fee28e6b6d9d9c6dd71d8b7c225f95e57869
Aleprosli <aliff.rosli96@gmail.com>
2022-03-03 15:25:03 +0800

    input file

--- resources/views/livewire/posts/create.blade.php
+++ resources/views/livewire/posts/create.blade.php
@@ -22,12 +22,31 @@
                     <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" wire:model="description" placeholder="Enter Description"></textarea>
                     @error('description') <span class="text-red-500">{{ $message }}</span>@enderror
                 </div>
+                <div class="mb-4">
+                  <label for="formFile" class="form-label inline-block mb-2 text-gray-700">File Input</label>
+                    <input class="form-control
+                    block
+                    w-full
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" wire:model="image" id="image">
+                  @error('image') <span class="text-red-500">{{ $message }}</span>@enderror
+              </div>
           </div>
         </div>
     
         <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
           <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-            <button wire:click.prevent="storeWithValidation()" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-black shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+            <button wire:click.prevent="storeWithImage()" enctype="multipart/form-data" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-black shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
               Save
             </button>
           </span>
