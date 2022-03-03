bff135bade7912cb22bde595007c5cc9b06098ed
Aleprosli <aliff.rosli96@gmail.com>
2022-03-03 13:53:50 +0800

    add validation rules, add storeWithValidation function

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -46,6 +46,12 @@ class Posts extends Component
         $this->isOpen = false;
     }
 
+    //validation rules
+    protected $rules = [
+        'title' => 'required|min:6',
+        'description' => 'required',
+    ];
+    
     public function store()
     {
         //updateorcreate, check id, and column declared on top
@@ -60,6 +66,29 @@ class Posts extends Component
         //return message
         session()->flash('message', 
             $this->post_id ? 'Post Updated Successfully.' : 'Post Created Successfully.');
+  
+        //call closemodal
+        $this->closeModal();
+
+        //call resetform
+        $this->resetInputFields();
+    }
+
+    public function storeWithValidation()
+    {
+        $this->validate();
+ 
+        // Execution doesn't reach here if validation fails
+        Post::updateOrCreate(
+            ['id' => $this->post_id],
+            [
+            'title' => $this->title,
+            'description' => $this->description
+            ]
+        );
+
+        //return message
+        session()->flash('message', 'Post Created Successfully.');
   
         //call closemodal
         $this->closeModal();
