cf300ca93abbd72c0afe916ed2ae2778244d526f
Aleprosli <aliff.rosli96@gmail.com>
2022-03-03 11:22:26 +0800

    add button create and input field

--- app/Http/Livewire/Post.php
+++ app/Http/Livewire/Post.php
@@ -6,8 +6,34 @@ use Livewire\Component;
 
 class Post extends Component
 {
+
+    public $isOpen = 0;
+
     public function render()
     {
         return view('livewire.post');
+    }
+
+    public function create()
+    {
+        $this->resetInputFields();
+        $this->openModal();
+    }
+
+    private function resetInputFields()
+    {
+        $this->title = '';
+        $this->description = '';
+        $this->post_id = '';
+    }
+
+    public function openModal()
+    {
+        $this->isOpen = true;
+    }
+
+    public function closeModal()
+    {
+        $this->isOpen = false;
     }
 }
